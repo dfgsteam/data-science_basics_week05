6cdce4c9e0d5eb0fc0ca5bb14678b817108e5a6b
Julius Hunold [mac] <info@julius-hunold.de>
2024-06-10 10:26:23 +0200

    update mergesort plotting

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,88 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
 import matplotlib.pyplot as plt
+import numpy as np
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+def assign_value(destination_list, dest_index, source_list, src_index):
+    """
+    Assigns the value from source_list[src_index] to destination_list[dest_index].
+
+    Args:
+        destination_list (list): The list to which the value is assigned.
+        dest_index (int): The index in the destination list where the value is assigned.
+        source_list (list): The list from which the value is taken.
+        src_index (int): The index in the source list from which the value is taken.
+    """
+    destination_list[dest_index] = source_list[src_index]
+
+def merge_sort(input_list):
+    """
+    Sorts a list in ascending order using the merge sort algorithm.
+
+    Args:
+        input_list (list): The list to be sorted.
+
+    Returns:
+        list: The sorted list.
+    """
+    if len(input_list) > 1:
+        mid = len(input_list) // 2
+        left_half = input_list[:mid]
+        right_half = input_list[mid:]
+
+        # Recursively sort both halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # Merge the sorted halves
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] <= right_half[j]:
+                input_list[k] = left_half[i]
+                i += 1
+            else:
+                input_list[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Copy remaining elements of left_half, if any
+        while i < len(left_half):
+            input_list[k] = left_half[i]
+            i += 1
+            k += 1
+
+        # Copy remaining elements of right_half, if any
+        while j < len(right_half):
+            input_list[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return input_list
+
+def visualize_sorting(input_list):
+    """
+    Visualizes the sorting process using matplotlib.
+
+    Args:
+        input_list (list): The list to be sorted and visualized.
+    """
+    x = np.arange(len(input_list))
+
+    # Sort the list
+    sorted_list = merge_sort(input_list.copy())
+
+    # Plot the unsorted and sorted list in the same diagram
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, input_list, label='Unsorted', color='blue', marker='o')
+    plt.plot(x, sorted_list, label='Sorted', color='green', marker='o')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.title('Visualization of Merge Sort')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+# Example usage
+if __name__ == "__main__":
+    example_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    visualize_sorting(example_list)
+
